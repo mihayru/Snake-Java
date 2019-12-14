6c782616cc042521d6377e430abe0e6535311fb8
mihayru <mjgang258@gmail.com>
2019-12-14 14:02:28 +0200

    Почти сделано

--- src/GameField.java
+++ src/GameField.java
@@ -1,11 +1,13 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.ImageObserver;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.*;
 import java.util.Random;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GameField extends JPanel  implements ActionListener {
+public class GameField extends JPanel implements ActionListener {
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
     private final int ALL_DOTS = 400;
@@ -13,8 +15,8 @@ public class GameField extends JPanel  implements ActionListener {
     private Image apple;
     private int appleX;
     private int appleY;
-    private int[] x = new int[ALL_DOTS];
-    private int[] y = new int[ALL_DOTS];
+    private int[] x = new int[400];
+    private int[] y = new int[400];
     private int dots;
     private Timer timer;
     private boolean left = false;
@@ -23,108 +25,152 @@ public class GameField extends JPanel  implements ActionListener {
     private boolean down = false;
     private boolean inGame = true;
 
-    public GameField(){
-        setBackground(Color.black);
-        loadImages();
-        initGame();
-
+    public GameField() {
+        this.setBackground(Color.black);
+        this.loadImages();
+        this.initGame();
+        this.addKeyListener(new FieldKeyListener());
+        this.setFocusable(true);
     }
 
-    public void initGame(){
-        dots = 3;
-        for (int i = 0; i < dots; i++) {
-            x[i] = 48 - i*DOT_SIZE;
-            y[i] = 48;
+    public void initGame() {
+        this.dots = 3;
+
+        for(int i = 0; i < this.dots; ++i) {
+            this.x[i] = 48 - i * 16;
+            this.y[i] = 48;
         }
 
-        timer = new Timer(250,this);
-        timer.start();
-        createApple();
-        
+        this.timer = new Timer(250, this);
+        this.timer.start();
+        this.createApple();
     }
 
-    private void createApple() {
-        appleX = new Random().nextInt(20)*DOT_SIZE;
-        appleY = new Random().nextInt(20)*DOT_SIZE;
+    public void createApple() {
+        this.appleX = (new Random()).nextInt(20) * 16;
+        this.appleY = (new Random()).nextInt(20) * 16;
     }
 
-    public void loadImages(){
+    public void loadImages() {
         ImageIcon iia = new ImageIcon("apple.png");
-        apple = iia.getImage();
+        this.apple = iia.getImage();
         ImageIcon iid = new ImageIcon("dot.png");
-        dot = iid.getImage();
-
+        this.dot = iid.getImage();
     }
 
-    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if(inGame){
-            g.drawImage(apple,appleX,appleY,this);
-            for (int i = 0; i < dots; i++) {
-                g.drawImage(dot,x[i],y[i],this);
+        if (this.inGame) {
+            g.drawImage(this.apple, this.appleX, this.appleY, this);
 
+            for(int i = 0; i < this.dots; ++i) {
+                g.drawImage(this.dot, this.x[i], this.y[i], this);
             }
-
-        }
-    }
-
-    public void move(){
-        for (int i = dots; i >0 ; i--) {
-            x[i] = x [i-1];
-            y[i] = y [i-1];
-        }
-        if (left){
-            x[0] -= DOT_SIZE;
-        }
-        if (right){
-            x[0] += DOT_SIZE;
-        }
-        if (up){
-            y[0] -= DOT_SIZE;
-        }
-        if (down){
-            y[0] -= DOT_SIZE;
+        } else {
+            String str = "Game Over";
+            g.setColor(Color.white);
+            g.drawString(str, 125, 160);
         }
 
     }
 
-    public void checkApple(){
-        if(x[0] == appleX && y[0] == appleY){
-            dots++;
-            createApple();
+    public void move() {
+        for(int i = this.dots; i > 0; --i) {
+            this.x[i] = this.x[i - 1];
+            this.y[i] = this.y[i - 1];
         }
+
+        int[] var10000;
+        if (this.left) {
+            var10000 = this.x;
+            var10000[0] -= 16;
+        }
+
+        if (this.right) {
+            var10000 = this.x;
+            var10000[0] += 16;
+        }
+
+        if (this.up) {
+            var10000 = this.y;
+            var10000[0] -= 16;
+        }
+
+        if (this.down) {
+            var10000 = this.y;
+            var10000[0] += 16;
+        }
+
     }
 
-    public void checkCollisions(){
-        for (int i =dots; i >0 ; i--) {
-            if(i>4 && x[0] == x[i] && y[0] == y[i]){
-                inGame = false;
+    public void checkApple() {
+        if (this.x[0] == this.appleX && this.y[0] == this.appleY) {
+            ++this.dots;
+            this.createApple();
+        }
 
+    }
+
+    public void checkCollisions() {
+        for(int i = this.dots; i > 0; --i) {
+            if (i > 4 && this.x[0] == this.x[i] && this.y[0] == this.y[i]) {
+                this.inGame = false;
             }
         }
-        if(x[0]>SIZE){
-            inGame = false;
+
+        if (this.x[0] > 320) {
+            this.inGame = false;
         }
-        if(x[0]<0){
-            inGame = false;
+
+        if (this.x[0] < 0) {
+            this.inGame = false;
         }
-        if(y[0]>SIZE){
-            inGame = false;
+
+        if (this.y[0] > 320) {
+            this.inGame = false;
         }
-        if(y[0]<0){
-            inGame = false;
+
+        if (this.y[0] < 0) {
+            this.inGame = false;
         }
+
     }
 
-    @Override
     public void actionPerformed(ActionEvent e) {
-        if(inGame){
-            move();
-            checkApple();
-            checkCollisions();
+        if (this.inGame) {
+            this.checkApple();
+            this.checkCollisions();
+            this.move();
+        }
+
+        this.repaint();
+    }
+
+    public class FieldKeyListener extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            int key = e.getKeyCode();
+            if(key == KeyEvent.VK_LEFT && !right){
+                left = true;
+                up = false;
+                down = false;
+            }if(key == KeyEvent.VK_RIGHT && !left){
+                right = true;
+                up = false;
+                down = false;
+            }
+            if(key == KeyEvent.VK_UP && !down){
+                right = false;
+                up = true;
+                left = false;
+            }
+            if(key == KeyEvent.VK_DOWN && !up){
+                right = false;
+                down = true;
+                left = false;
+            }
 
         }
-        repaint();
     }
 }
